5697aaa38c882dfc6e63337700b0c544cdf14ede
Carlos Cueto <crcueto25@gmail.com>
2023-06-19 12:52:34 +0300

    Add exception for emergency break

--- crazyflie_examples/crazyflie_examples/hello_world.py
+++ crazyflie_examples/crazyflie_examples/hello_world.py
@@ -9,15 +9,22 @@ HOVER_DURATION = 5.0
 
 
 def main():
-    swarm = Crazyswarm()
-    timeHelper = swarm.timeHelper
-    cf = swarm.allcfs.crazyflies[0]
+    try:
+        swarm = Crazyswarm()
+        timeHelper = swarm.timeHelper
+        cf = swarm.allcfs.crazyflies[0]
 
-    cf.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
-    timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
-    cf.land(targetHeight=0.04, duration=2.5)
-    timeHelper.sleep(TAKEOFF_DURATION)
-
+        cf.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
+        timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
+        cf.land(targetHeight=0.04, duration=2.5)
+        timeHelper.sleep(TAKEOFF_DURATION)
+    except KeyboardInterrupt:
+        # Press Ctrl-C to stop the program and land all CFs
+        pass
+    finally:
+        if swarm is not None:
+            cf.land(targetHeight=0.04, duration=2.5)
+            timeHelper.sleep(TAKEOFF_DURATION)
 
 if __name__ == "__main__":
     main()
